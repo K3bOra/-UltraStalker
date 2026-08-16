2e0538ed79a7ac1f515c0922a5378d23681cc545
K3bOra <k3borazz0@gmail.com>
2026-08-16 09:50:20 +0300

    Update IPK_NAME for UltraStalker installation

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 set -u
 
 PLUGIN_PKG="enigma2-plugin-extensions-ultrastalker"
-IPK_NAME="UltraStalker_FINAL_Public_Production_Dependencies_all.ipk"
+IPK_NAME="UltraStalker_FINAL_Public_all.ipk"
 # CHANGE THIS after uploading the IPK to your GitHub Release:
 IPK_URL="https://github.com/K3bOra/-UltraStalker/releases/latest/download/${IPK_NAME}"
 TMP_IPK="/tmp/${IPK_NAME}"
