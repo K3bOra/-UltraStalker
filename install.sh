ebcd9b03ebe5b9d37420292043d927537651a973
K3bOra <k3borazz0@gmail.com>
2026-08-22 15:26:27 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@
 set -u
 
 PLUGIN_PKG="enigma2-plugin-extensions-ultrastalker"
-IPK_NAME="UltraStalker_V4.3.6_SmartImport_all.ipk"
+IPK_NAME="UltraStalker_V4.3.7_SmartDependencies_all.ipk"
 # CHANGE THIS after uploading the IPK to your GitHub Release:
 IPK_URL="https://github.com/K3bOra/-UltraStalker/releases/latest/download/${IPK_NAME}"
 TMP_IPK="/tmp/${IPK_NAME}"
